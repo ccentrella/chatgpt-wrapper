fa57651d7dc40e60173f136acd525d96e9d8c478
Christopher Centrella <ccentrella.j@gmail.com>
2024-09-27 22:20:38 -0500

    refactor code structure

--- chatgpt.py
+++ chatgpt.py
@@ -2,31 +2,38 @@ import os
 import sys
 from openai import OpenAI
 
-client = OpenAI()
-try:
-    with open('..instructions.txt', 'r') as file:
-        instructions = file.read()
-except FileNotFoundError:
-    instructions = ''
+def send_request():
+    instructions, model, prompt = load_information()
+    client = OpenAI()
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": instructions
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    print(completion.choices[0].message.content)
 
-prompt = " ".join(sys.argv[1:])
-if prompt.strip() == '':
-    raise AttributeError("Prompt is required")
-model = os.environ.get('MODEL') or 'gpt-4o-mini'
+def load_information():
+    try:
+        with open('..instructions.txt', 'r') as file:
+            instructions = file.read()
+    except FileNotFoundError:
+        instructions = ''
 
-print('\nWaiting for response from ChatGPT...')
+    model = os.environ.get('MODEL') or 'gpt-4o-mini'
+    prompt = " ".join(sys.argv[1:])
+    if prompt.strip() == '':
+        raise AttributeError("Prompt is required")
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {   
-            "role": "system",
-            "content": instructions
-        },
-        {
-            "role": "user",
-            "content": prompt
-        }
-    ]
-)
-print(completion.choices[0].message.content)
+    print('\nWaiting for response from ChatGPT...')
+
+    return instructions, model, prompt
+
+send_request()
