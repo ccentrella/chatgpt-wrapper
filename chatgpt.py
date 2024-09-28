7d1b067ca52390127dfa7f8539276b0e487fe5cb
Christopher Centrella <ccentrella.j@gmail.com>
2024-09-27 21:45:36 -0500

    initial commit

---
+++ chatgpt.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from openai import OpenAI
+
+client = OpenAI()
+try:
+    with open('..instructions.txt', 'r') as file:
+        instructions = file.read()
+except FileNotFoundError:
+    instructions = ''
+
+prompt = " ".join(sys.argv[1:])
+if prompt.strip() == '':
+    raise AttributeError("Prompt is required")
+model = os.environ.get('MODEL') or 'gpt-4o-mini'
+
+print('Waiting for response from ChatGPT...')
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {   
+            "role": "system",
+            "content": instructions
+        },
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+print(completion.choices[0].message.content)
